270bab352df93fc2d554b4dcf477c154c4a438e2
Rebecca Fleak <fleakrl@gmail.com>
2017-03-23 18:37:12 -0500

    implemented studio-2 Flicklist-2
    - cleaned up a little bit of the code

--- Studios/flicklist-python-studio2/main.py
+++ Studios/flicklist-python-studio2/main.py
@@ -1,31 +1,45 @@
 import webapp2
 
+header = """
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Flicklist</flicklist>
+</head>
+
+<body>
+    <h1>Flicklist</h1>
+"""
+footer = """
+</body>
+</html>
+"""
+
 class Index(webapp2.RequestHandler):
 
     def get(self):
-        header = """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>Flicklist</flicklist>
-                </head>
-                <body>
-                    <h1>Flicklist</h1>
+          form = """
+          <h3> Edit My Watchlist </h3>
 
-                    <h3> Edit My Watchlist </h3>
-
-                    <form action ='/add' method = 'post'>
-                        <label>
-                            I would like to add:
-                            <input type='text' name='new_movie'>
-                            to my watchlist.
-                        </label>
-
-
-                </body>
-            </html>
+          <form action ='/add' method = 'post'>
+            <label> I would like to add
+                <input type='text' name='new_movie'/>
+                to my watchlist.
+            </label>
+            <input type = 'submit' value = 'Add it'>
+         </form>
         """
-        self.response.write(header)
+            self.response.write(header + form + footer)
+
+class AddMovie(webapp2.RequestHandler):
+
+    def post(self):
+        movie_name = self.request.get('new_movie')
+
+        success_message = "<strong>" + movie_name + "</strong> has been added to your list. (but not really)"
+
+        self.response.write(header + success_message+ footer)
 
 app = webapp2.WSGIApplication([
     ('/', Index)
