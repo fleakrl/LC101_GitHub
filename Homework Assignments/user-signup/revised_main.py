f05b307baff45b87fc0f987473a1de2e8e7fa690
Rebecca Fleak <fleakrl@gmail.com>
2017-04-03 22:17:53 -0500

    User Signup: revised user form & started a revised main file from scratch

---
+++ Homework Assignments/user-signup/revised_main.py
@@ -0,0 +1,20 @@
+"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Signup</title>
+        <style type="text/css">
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <form>
+        <table>
+        </table>
+    
+    </body>
+</html>
+"""
