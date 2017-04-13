cac854b0c5d2e963300fb72bdcb4f33783be391a
Rebecca Fleak <fleakrl@gmail.com>
2017-04-12 21:47:48 -0500

    error handling on input form is working

--- Homework Assignments/build-a-blog/blogclasses/NewPostHandlerClass.py
+++ Homework Assignments/build-a-blog/blogclasses/NewPostHandlerClass.py
@@ -11,19 +11,36 @@ class NewPostHandler(webapp2.RequestHandler):
     """ Handles requests coming in to '/new-post' 
     """
     def get(self):
+        error = self.request.get('error')
+        title = self.request.get('title')
+        content=self.request.get('content')
+        titleerror = self.request.get('titleerror')
+        contenterror = self.request.get('contenterror')
+
+
         t = jinja_env.get_template("new_blog_post.html")
-        content = t.render()
+        content = t.render(error=error, title=title, content = content, titleerror = titleerror, contenterror = contenterror)
         self.response.write(content)
 
     def post(self):
         new_post_title = self.request.get("post_title")
         new_post_content = self.request.get("post_content_text")
+        titleerror = ""
+        contenterror = ""
+
 
         # if the user typed nothing at all, redirect and yell at them
-        if (not new_post_title) or (not new_post_content) or \
-                (new_post_title.strip() == "") or (new_post_content.strip() == ""):
-            error = "All fields are required."
-            self.redirect("/new-post?error=" + cgi.escape(error))
+        if (not new_post_title) or (new_post_title.strip() == "") or (not new_post_content) or (new_post_content.strip() == ""):
+
+            if (not new_post_title) or (new_post_title.strip() == ""):
+                titleerror = "title fields is required."
+
+            if (not new_post_content) or (new_post_content.strip() == ""):
+                contenterror = "content field is required."
+
+            return self.redirect("/new-post?title="
+                                 + new_post_title + "&content=" + new_post_content
+                                 + "&titleerror=" + titleerror + "&contenterror=" + contenterror)
 
         """ Adds a new blog post to the database & redirects to main page """
         blog_post = BlogPostClass.BlogPost(title = new_post_title, content = new_post_content)
