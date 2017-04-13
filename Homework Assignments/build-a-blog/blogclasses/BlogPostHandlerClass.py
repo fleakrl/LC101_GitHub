e9aa751129f2d3b29f94852054db942776deded7
Rebecca Fleak <fleakrl@gmail.com>
2017-04-12 19:49:08 -0500

    /blog-post/<blogpostid> path is finally working!

--- Homework Assignments/build-a-blog/blogclasses/BlogPostHandlerClass.py
+++ Homework Assignments/build-a-blog/blogclasses/BlogPostHandlerClass.py
@@ -2,6 +2,8 @@ import webapp2
 import setup_jinja
 from google.appengine.ext import db
 
+from blogclasses.BlogPostClass import BlogPost
+
 jinja_env = setup_jinja.setup_jinja()
 
 
@@ -13,15 +15,18 @@ class BlogPostHandler(webapp2.RequestHandler):
         """ Displays a list of all blog posts if no blog post id is submitted
             If a blog post id is submitted only shows the blog post for the specified ID
         """
-        if blogpostid == None:
+        print(blogpostid)
+
+        if blogpostid is None:
             blogposts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC")
             t = jinja_env.get_template("master_list_table.html")
             content = t.render(blogposts=blogposts)
             self.response.write(content)
         else:
-            blogpostid = int(blogpostid)
-            blogposts = db.GqlQuery("SELECT * FROM BlogPost WHERE id IS blogpostid")
-            t = jinja_env.get_template("master_list_table.html")
-            content = t.render(blogposts=blogposts)
-            self.response.write(content)
+            blogposts = BlogPost.get_by_id(int(blogpostid))
 
+            blogpostlist = []
+            blogpostlist.append(blogposts)
+            t = jinja_env.get_template("master_list_table.html")
+            content = t.render(blogposts=blogpostlist)
+            self.response.write(content)
